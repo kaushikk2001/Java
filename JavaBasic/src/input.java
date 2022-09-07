8fb00852f8e356e7e2bf37fdd0d437857eba039c
kaushik khadilkar <77961998+kaushikk2001@users.noreply.github.com>
2022-09-08 01:14:03 +0530

    Add files via upload

---
+++ JavaBasic/src/input.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+     Scanner sc =new Scanner(System.in);
+     int A=sc.nextInt();//To take Integer input
+     Float B=sc.nextFloat();//To take Float input
+     String C=sc.nextLine();//To take String input
+     System.out.printf(A+B+C);
+     System.out.println(A);
+     System.out.println(B);
+     System.out.println(C);
+    }
+}
