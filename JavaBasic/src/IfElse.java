796364f95b2dedb2bddb8328630c2f01a952b92c
kaushik khadilkar <77961998+kaushikk2001@users.noreply.github.com>
2022-09-08 01:19:35 +0530

    Add files via upload

---
+++ JavaBasic/src/IfElse.java
@@ -0,0 +1,15 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("The Greater Number is " +a);
+        }
+        else if(a<b){
+            System.out.println("The greater Number is "+b);
+        }
+        else{
+            System.out.println("Bothe Number "+a +b +"are Same");
+        }
+    }
+}
