9e2dfe13b35871c78b9724b310072f4c65a27821
kaushik khadilkar <77961998+kaushikk2001@users.noreply.github.com>
2022-09-08 23:50:34 +0530

    Add files via upload

---
+++ JavaBasic/src/SwitchBreak.java
@@ -0,0 +1,13 @@
+public class SwitchBreak {
+    public static void main(String[] args) {
+        int Button=2;
+        switch(Button)
+        {
+            case 1:System.out.println("Button 1 pressed");//Case 1
+            break;
+            case 2:System.out.println("Button 2 pressed");//case 2
+            break;
+            default:System.out.println("No button Pressent");//default case
+        }
+    }
+}
