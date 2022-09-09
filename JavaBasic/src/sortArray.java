f530cb979b3528128d7676d3374f3a8ebfb21248
kaushik khadilkar <77961998+kaushikk2001@users.noreply.github.com>
2022-09-09 13:08:14 +0530

    Add files via upload

---
+++ JavaBasic/src/sortArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String[] args) {
+        int[] A={1,5,3,7,8};
+        Arrays.sort(A);//use to sort the array
+        for(int i=0;i<5;i++){
+            System.out.print(A[i] +" ");
+        }
+    }
+    
+}
